2c59ad351e6b4aaf3327feadd69db65cba0fc919
alex <alex@localhost>
2025-11-04 20:33:55 -0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://alesh4.github.io',
-	base: 'blog',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
